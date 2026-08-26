4ce801e66ef6f110ac051fa0bafa50c0aa8636c8
ksp012 <inseuton19@gmail.com>
2026-08-26 11:16:11 +0900

    테스트사이즈

--- imagescut.py
+++ imagescut.py
@@ -1,16 +1,19 @@
 from PIL import Image
 import os
 
-folder_path = './images' # 네 이미지 폴더 경로
+folder_path = './images'
 for filename in os.listdir(folder_path):
     if filename.endswith(".png") or filename.endswith(".jpg"):
         img_path = os.path.join(folder_path, filename)
         img = Image.open(img_path)
         
-        # 가로 400 기준으로 비율 맞춰서 줄이기
-        width = 400
+        # 가로 500 픽셀로 설정 (적당한 크기와 화질 타협점)
+        width = 500 
         ratio = (width / float(img.size[0]))
         height = int((float(img.size[1]) * float(ratio)))
         
-        resized_img = img.resize((width, height))
-        resized_img.save(img_path) # 원본 덮어쓰기
+        # LANCZOS 필터 적용해서 초고화질로 리사이징!
+        resized_img = img.resize((width, height), Image.Resampling.LANCZOS)
+        
+        # PNG 퀄리티 손실 없이 덮어쓰기
+        resized_img.save(img_path, quality=95)
